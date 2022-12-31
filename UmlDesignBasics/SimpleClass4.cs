2b27f46ac9beacf925c070858ebde835ce7ac6f4
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-12-31 11:44:05 +0300

    Mute SA1401, S1104 and CA1051.

--- UmlDesignBasics/SimpleClass4.cs
+++ UmlDesignBasics/SimpleClass4.cs
@@ -1,3 +1,7 @@
+#pragma warning disable SA1401 // Field should be private
+#pragma warning disable S1104  // Fields should not have private accessibility
+#pragma warning disable CA1051 // Do not declare visible instance fields
+
 // ReSharper disable InconsistentNaming
 namespace UmlDesignBasics
 {
